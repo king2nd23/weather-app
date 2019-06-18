09352b7064cd5e12f50000c2bb13395a04ecdcf3
Mark King <king2nd23@gmail.com>
2019-06-18 15:52:23 -0400

    still working on getting commits to work

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Testings
+          Testing
         </p>
         <a
           className="App-link"
