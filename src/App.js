b954b43d0c481553e95fead3c53a18278d00b52f
Mark King <king2nd23@gmail.com>
2019-06-18 15:46:06 -0400

    Trying to get changes to publish to gh

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Testing 1, 2, 3
+          Testing...
         </p>
         <a
           className="App-link"
