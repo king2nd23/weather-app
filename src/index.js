dbbbd8a7e72a0aa6252c21ea63360029f1b04486
Mark King <king2nd23@gmail.com>
2019-06-19 15:30:22 -0400

    npw have data from weather api

--- src/index.js
+++ src/index.js
@@ -1,12 +1,64 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class App extends React.Component {
+  state = { lat: null, long: null, errormsg: "" };
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+  componentDidMount() {
+    const getPosition = () => {
+      window.navigator.geolocation.getCurrentPosition(
+        position => {
+          this.setState({
+            lat: position.coords.latitude,
+            long: position.coords.longitude
+          });
+        },
+        error => this.setState({ errormsg: error.message })
+      );
+    };
+    getPosition();
+
+    const getWeatherData = () => {
+      let apiCall = `https://api.darksky.net/forecast/c2f9808ca1a73ccc2367f463c969facc/${
+        this.state.lat
+      },${this.state.long}`;
+      fetch(apiCall)
+        .then(res => res.json())
+        .then(response => {
+          let string = JSON.stringify(response.daily.data);
+          let parsed = JSON.parse(string);
+          let iconArray = parsed.map((item) => {
+            return item.icon;
+          });
+          console.log(iconArray);
+          console.log(parsed);
+          console.log(response);
+        })
+        .catch(error => console.log(error));
+      console.log(apiCall);
+    };
+
+    function getWeather() {
+      setTimeout(() => {
+        getWeatherData();
+      }, 4000);
+    }
+    getWeather();
+  }
+
+
+
+
+
+
+  ///////
+  render() {
+    return (
+      <div>
+        <p>Hello</p>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.querySelector("#root"));
