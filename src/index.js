9f0fb533729cb821cff7afd19a15c1532cb61e9c
Mark King <king2nd23@gmail.com>
2019-06-27 16:15:52 -0400

    components are now updating with state

--- src/index.js
+++ src/index.js
@@ -4,7 +4,18 @@ import CurrentWeather from "./CurrentWeather";
 import WeatherIcons from 'react-weathericons';
 
 class App extends React.Component {
-  state = { lat: null, long: null, errormsg: "" };
+  state = {
+    lat: null,
+    long: null,
+    errormsg: "",
+    icon: null,
+    currentTemp: null,
+    high: null,
+    low: null,
+    wind: null,
+    chanceOfRain: null,
+    humidity: null
+  };
 
   componentDidMount() {
     const getPosition = () => {
@@ -20,48 +31,114 @@ class App extends React.Component {
     };
     getPosition();
 
-    const getWeatherData = () => {
-      let apiCall = `https://api.darksky.net/forecast/c2f9808ca1a73ccc2367f463c969facc/${
-        this.state.lat
-      },${this.state.long}`;
-      fetch(apiCall)
-        .then(res => res.json())
-        .then(response => {
-          //get weekly weather forecast
-          let dailyString = response.daily.data;
-          let iconArray = dailyString.map((item) => {
-            return item.icon;
-          });
-          //get current weather conditions
-          let currentWeather = response.currently;
-          let currentWind = currentWeather.windSpeed;
-          let chanceOfRain = currentWeather.precipProbability;
-          let currentHumidity = currentWeather.humidity;
-          let currentIcon = currentWeather.icon;
-          let currentTemp = currentWeather.apparentTemperature;
-          //get high and low //
-          let highTemp = response.daily.data[0].temperatureHigh;
-          let lowTemp = response.daily.data[0].temperatureLow;
-          console.log(currentWeather);
-        })
-        .catch(error => console.log(error));
-      console.log(apiCall);
-    };
-    function getWeather() {
-      setTimeout(() => {
-        getWeatherData();
-      }, 4000);
-    }
-    getWeather();
+/////////////working on async function//////////////////////////////////////////
+let async1 = ()=> {
+  let apiCall = `https://api.darksky.net/forecast/c2f9808ca1a73ccc2367f463c969facc/${
+    this.state.lat
+  },${this.state.long}`;
+  return apiCall;
+}
+
+let async2 = async(url) => {
+  fetch(url)
+  .then(res => res.json())
+  .then(response => {
+        //get current weather conditions
+        // let currentWeather = response.currently;
+        // let currentWind = currentWeather.windSpeed;
+        // let chanceOfRain = currentWeather.precipProbability;
+        // let currentHumidity = currentWeather.humidity;
+        // let currentIcon = currentWeather.icon;
+        // let currentTemp = currentWeather.apparentTemperature;
+        // //get high and low //
+        // let highTemp = response.daily.data[0].temperatureHigh;
+        // let lowTemp = response.daily.data[0].temperatureLow;
+        let currentWeather = response.currently;
+        this.setState({currentWind: currentWeather.windSpeed});
+        this.setState({chanceOfRain: currentWeather.precipProbability});
+        this.setState({currentHumidity: currentWeather.humidity});
+        this.setState({currentIcon: currentWeather.icon});
+        this.setState({currentTemp: currentWeather.apparentTemperature});
+        this.setState({highTemp: response.daily.data[0].temperatureHigh});
+        this.setState({lowTemp: response.daily.data[0].temperatureLow});
+        console.log(currentWeather);
+        console.log(response);
+      })
+      .catch(error => console.log(error))
+}
+
+
+
+
+let async3 = async(response) => {
+}
+const doManyThings = async () => {
+  async1();
+  console.log(async1());
+  await async2(async1());
+  // await async3();
+  console.log(this.state);
+};
+
+
+function getWeather() {
+    setTimeout(() => {
+      doManyThings();
+    }, 4000)
+  }
+  getWeather();
+
+
+////////////working on aync function//////////////////////////////////////////
+    // const getWeatherData = () => {
+    //   let apiCall = `https://api.darksky.net/forecast/c2f9808ca1a73ccc2367f463c969facc/${
+    //     this.state.lat
+    //   },${this.state.long}`;
+    //   fetch(apiCall)
+        // .then(res => res.json())
+        // .then(response => this.setState({
+        //   //get current weather conditions
+        //   currentWeather: response.currently,
+        //   currentWind: currentWeather.windSpeed,
+        //   chanceOfRain: currentWeather.precipProbability,
+        //   currentHumidity: currentWeather.humidity,
+        //   currentIcon: currentWeather.icon,
+        //   currentTemp: currentWeather.apparentTemperature,
+        //   //get high and low //
+        //   highTemp: response.daily.data[0].temperatureHigh,
+        //   lowTemp: response.daily.data[0].temperatureLow
+        // })
+    //
+    //     .then(response => {
+    //       //get weekly weather forecast
+    //       let dailyString = response.daily.data;
+    //       let iconArray = dailyString.map((item) => {
+    //         return item.icon;
+    //       });
+    //       //get current weather conditions
+    //       let currentWeather = response.currently;
+    //       let currentWind = currentWeather.windSpeed;
+    //       let chanceOfRain = currentWeather.precipProbability;
+    //       let currentHumidity = currentWeather.humidity;
+    //       let currentIcon = currentWeather.icon;
+    //       let currentTemp = currentWeather.apparentTemperature;
+    //       //get high and low //
+    //       let highTemp = response.daily.data[0].temperatureHigh;
+    //       let lowTemp = response.daily.data[0].temperatureLow;
+    //       console.log(currentWeather);
+    //       console.log(response);
+    //     })
+    //     .catch(error => console.log(error))
+    // };
   }
 
 
   render() {
     return (
       <div>
-        <CurrentWeather icon={this.currentIcon} currentTemp={this.currentTemp}
-           high={this.highTemp} low={this.lowTemp} wind={this.currentWind}
-           chanceOfRain={this.chanceOfRain} humidity= "currentHumidity" />
+        <CurrentWeather icon={this.state.currentIcon} currentTemp={this.state.currentTemp}
+           high={this.state.highTemp} low={this.state.lowTemp} wind={this.state.currentWind}
+           chanceOfRain={this.state.chanceOfRain} humidity={this.state.currentHumidity} />
       </div>
     );
   }
